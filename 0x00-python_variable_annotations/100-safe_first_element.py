daf6efde6637992f6728e409ccf76a6946d8a95b
omwami <ianalpha@protonmail.com>
2023-10-10 21:13:11 +0300

    duck-typing Any

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Annotated module """
+from typing import Sequence, Any, Union, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
