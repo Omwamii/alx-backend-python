2bfbb76abbcee3643ab8a3300c23eccd9ed9c345
omwami <ianalpha@protonmail.com>
2023-10-11 12:48:16 +0300

    yield random nos

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,4 +8,5 @@ import random
 async def async_generator() -> AsyncGenerator[float, None]:
     """ yield random number """
     for _ in range(0, 10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
