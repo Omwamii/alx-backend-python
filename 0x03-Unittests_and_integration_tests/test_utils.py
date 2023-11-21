eddeb3540fdc385c851af4bcd36f76fed5231150
omwami <ianalpha@protonmail.com>
2023-11-21 11:17:45 +0300

    test Raises error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,16 +14,25 @@ from typing import (
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    ''' Test the utils.access_nested_map method
-    '''
+    """ Test the utils.access_nested_map method
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
         ])
     def test_access_nested_map(self, nested_map: Mapping, path: Sequence, result: Any):
-        ''' Test method '''
+        """ Test method """
         self.assertEqual(access_nested_map(nested_map, path), result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")
+         )])
+    def test_access_nested_map_exception(self, nested_map: Mapping, path: Sequence):
+        """ Test exception in access_nested_map """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == "__main__":
